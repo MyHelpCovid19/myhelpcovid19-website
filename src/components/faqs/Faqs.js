c4701345a49e0a1982b48ccc45ad4c41043d076e
karthikreddi <dkarthikreddy06@gmail.com>
2023-05-21 15:58:33 +0530

    new changes committed again

--- src/components/faqs/Faqs.js
+++ src/components/faqs/Faqs.js
@@ -44,7 +44,7 @@ const Faqs = () => {
       <Suspense fallback={<div>Loading...</div>}>
         {faqs.length > 0 ? (
           <div>
-            <div className="mb-2">FAQs About Us: and About you:</div>
+            <div className="mb-2">FAQs About Us:</div>
 
             <FaqLayout faqdata={faqs} />
           </div>
